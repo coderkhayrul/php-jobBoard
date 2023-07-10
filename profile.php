0443f984e83e1fd06e0e7dad21abe0032dbeea67
coderkhayrul <coderkhayrul@gmail.com>
2023-07-11 03:19:29 +0600

    Single Job Feature Working

--- profile.php
+++ profile.php
@@ -18,6 +18,11 @@ if (isset($_GET['id'])){
 }else{
     header("Location: " .  NOT_FOUND_URL, true, 301);
 }
+
+//All Jobs Show by User
+$jobs = $connection->query("SELECT * FROM jobs WHERE company_id = '$id' AND status = 1");
+$jobs->execute();
+$jobs = $jobs->fetchAll(PDO::FETCH_OBJ);
 ?>
 <section class="section-hero overlay inner-page bg-image" style="background-image: url('<?php BASE_URL ?>/public/images/hero_1.jpg');" id="home-section">
   <div class="container">
@@ -47,4 +52,41 @@ if (isset($_GET['id'])){
     </div>
   </div>
 </section>
+
+<section class="site-section">
+    <div class="container">
+        <div class="row mb-5 justify-content-center">
+            <div class="col-md-7 text-center">
+                <h2 class="section-title mb-2">43,167 Job Listed</h2>
+            </div>
+        </div>
+        <ul class="job-listings mb-5">
+            <?php foreach ($jobs as $job): ?>
+            <li class="job-listing d-block d-sm-flex pb-3 pb-sm-0 align-items-center">
+                <a href="<?php echo BASE_URL . '/job.php?id=' . $job->id ?>"></a>
+                <div class="job-listing-logo">
+                    <img src="<?php echo BASE_URL .'/'. $job->company_image ?>" alt="Free Website Template by Free-Template.co" class="img-fluid">
+                </div>
+
+                <div class="job-listing-about d-sm-flex custom-width w-100 justify-content-between mx-4">
+                    <div class="job-listing-position custom-width w-50 mb-3 mb-sm-0">
+                        <h2><?php echo $job->job_title ?></h2>
+                        <strong><?php echo $job->company_name ?></strong>
+                    </div>
+                    <div class="job-listing-location mb-3 mb-sm-0 custom-width w-25">
+                        <span class="icon-room"></span> <?php echo $job->job_region ?>
+                    </div>
+                    <div class="job-listing-meta">
+                        <span class="badge badge-danger"><?php echo $job->job_type ?></span>
+                    </div>
+                </div>
+            </li>
+            <?php endforeach; ?>
+        </ul>
+
+
+
+    </div>
+</section>
+
 <?php require "includes/footer.php"; ?>
